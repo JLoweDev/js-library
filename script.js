e25fd3422112ffc556b1445cd93585e808ed6649
James <67014970+JLoweDev@users.noreply.github.com>
2020-12-16 18:24:40 +0000

    Function to add book to array added

--- script.js
+++ script.js
@@ -5,11 +5,8 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-      return `${title} by ${author}, ${pages} pages, ${read}`;
-    }
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  myLibrary.push();
 }
