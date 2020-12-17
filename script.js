52c53a636a5ad758c9fc2547b29731ca5e6854ca
James <67014970+JLoweDev@users.noreply.github.com>
2020-12-17 20:43:39 +0000

    Add Book button now clears and can create empty cells

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ function render() {
   const table = document.getElementById("tableid");
   const tableitems = document.querySelectorAll(".tableitem");
   tableitems.forEach(tableitem => table.deleteRow(-1));
+  for (let i=0; i<myLibrary.length; i++){
+    renderBook(myLibrary[i]);
+  }
+}
+
+function renderBook(book) {
+  const table = document.getElementById("tableid");
+  const row = table.insertRow(myLibrary.indexOf(book)+1);
+  const titlecell = row.insertCell(0);
+  const authorcell = row.insertCell(1);
+  const pagescell = row.insertCell(2);
+  const readcell = row.insertCell(3);
+  const deletecell = row.insertCell(4);
+
+
 }
 
 function addBookToLibrary() {
